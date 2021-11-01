23b2850db405e174e45098a6aa42c6c7ea1cd35b
Abdellatif <23284113+abdelhousni@users.noreply.github.com>
2021-11-01 16:02:33 +0100

    alternative install neofetch for colored MoTD

---
+++ install_neofetch_alternative.sh
@@ -0,0 +1,7 @@
+
+# source : https://www.putorius.net/custom-motd-login-screen-linux.html
+
+# https://github.com/dylanaraps/neofetch/wiki/Installation#fedora--rhel--centos--mageia--openmandriva
+sudo dnf -y instal dnf-plugins-core
+sudo dnf install neofetch
+sudo dnf copr enable konimex/neofetch
