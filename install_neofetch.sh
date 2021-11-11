9844f5e72cc15874bbead24b689d7d3070860e1a
Abdellatif <23284113+abdelhousni@users.noreply.github.com>
2021-11-11 16:49:01 +0100

    salt sys.doc linux vs windows vs all

--- install_neofetch.sh
+++ install_neofetch.sh
@@ -2,6 +2,6 @@
 # source : https://www.putorius.net/custom-motd-login-screen-linux.html
 
 # https://github.com/dylanaraps/neofetch/wiki/Installation#fedora--rhel--centos--mageia--openmandriva
-sudo dnf -y instal dnf-plugins-core
+sudo dnf install dnf-plugins-core
 sudo dnf install neofetch
 sudo dnf copr enable konimex/neofetch
--- install_neofetch_alternative.sh
+++ install_neofetch_alternative.sh
@@ -2,6 +2,9 @@
 # source : https://www.putorius.net/custom-motd-login-screen-linux.html
 
 # https://github.com/dylanaraps/neofetch/wiki/Installation#fedora--rhel--centos--mageia--openmandriva
-sudo dnf -y instal dnf-plugins-core
+# 1. If you're using RHEL/CentOS, make sure you have installed epel-release
+# 2. Fetch the repo file
+curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
+  # NOTE: If you are using Fedora, change epel-7 to fedora-23 or your Fedora version respectively. However if you are using RHEL/CentOS 6, change it to epel-6.
+# 3 Install the package
 sudo dnf install neofetch
-sudo dnf copr enable konimex/neofetch
